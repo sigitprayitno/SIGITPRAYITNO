91d92ee27202ce6927e033c2cfadf7d0463d39b4
sigitprayitno <sigitprayitno12.01.2004@gmail.com>
2026-03-16 11:55:08 +0700

    update

--- js/script.js
+++ js/script.js
@@ -36,3 +36,25 @@ document.addEventListener('click',function(e){
     }
 })
 // Toogle class aktif untuk shopping cart//
+//modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display ='flex';
+        e.preventDefault();
+    };
+});
+//klik tombol close modal 
+document.querySelector ('.modal .close-icon') .onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+//klik di luar modal 
+window.onclick = (e) => {
+    if (e.target === itemDetailModal){
+        itemDetailModal.style.display='none';
+    }
+}
+
